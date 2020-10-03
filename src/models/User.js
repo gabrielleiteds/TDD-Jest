38b992febd11cb9a64aad2802149a5e958047750
leitegs <leitedasilvag@gmail.com>
2020-10-03 17:06:53 -0300

    create model for user

---
+++ src/models/User.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+const bcryptjs = require('bcryptjs');
+const uuid = require('uuid');
+
+//rounds for the hash of bcrypt
+const ROUNDS = 10;
+
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+}, {
+  hooks: {
+    afterValidate: async (user) => {
+      // generate uuid v4:
+      user.id = uuid.v4();
+
+      // encrypt the password:
+      if (user.password)
+        user.password = await bcryptjs.hash(user.password, ROUNDS);
+    }
+  },
+  sequelize,
+  tableName: 'users',
+  underscored: true
+});
+
+User.prototype.comparePassword = function (password) {
+  return bcryptjs.compare(password, this.password);
+}
+
+module.exports = User;
