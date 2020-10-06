c03992e1ddd9220dd7e26ad26da6ead672003f76
leitegs <leitedasilvag@gmail.com>
2020-10-05 22:54:54 -0300

    use dotenv

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+})
 const bodyParser = require('body-parser');
 const express = require('express');
 const routes = require('./routes');
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,16 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const env = process.env.NODE_ENV || 'test';
-const config = require('./config')[env];
+const config = require('./config');
 
 const connection = new Sequelize(config);
 
 try {
 	connection.authenticate();
-	console.log("conectado!");
+	console.log('conectado ao banco!');
 } catch (error) {
-	console.error("falha: ", error);
+	console.error('falha: ', error);
 }
 
 
-module.exports = connection
+module.exports = connection;
