bdf3a02f028d8eac388fdaf3a65458265d413bc6
leitegs <leitedasilvag@gmail.com>
2020-10-05 22:16:18 -0300

    configure for using jest

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,15 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const config = require('./config');
+const env = process.env.NODE_ENV || 'test';
+const config = require('./config')[env];
 
 const connection = new Sequelize(config);
 
 try {
 	connection.authenticate();
-	console.log('conectado ao banco!');
+	console.log("conectado!");
 } catch (error) {
-	console.error('falha: ', error);
+	console.error("falha: ", error);
 }
 
 
-module.exports = connection;
+module.exports = connection
