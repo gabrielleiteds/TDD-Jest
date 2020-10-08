1a499285e1e9ae4961190728a2849e44eabfafa8
leitegs <leitedasilvag@gmail.com>
2020-10-07 23:45:38 -0300

    create routes and use middleware for authentication

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 const { Router } = require('express')
 
+//controllers
+const SessionController = require('./controllers/SessionController')
+
+//middleware authentication
+const authMiddleware = require('./middleware/Auth')
+
 const routes = Router();
 
 //definindo rotas
-routes.get('/', (req, res) => {
-  res.send('hello')
+routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddleware) //se aplica apenas as que vem depois desta linha
+
+routes.get('/dashboard', (req, res) => {
+  return res.status(200).send()
 })
 
 module.exports = routes;
